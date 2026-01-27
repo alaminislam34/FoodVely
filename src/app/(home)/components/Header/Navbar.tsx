23a22a7fc7dceb80e0f96149d0110ca18654534c
alaminislam34 <mi3548514@gmail.com>
2026-01-28 03:02:02 +0600

    updated the sidebar make it fully responsive and functional

--- src/app/(home)/components/Header/Navbar.tsx
+++ src/app/(home)/components/Header/Navbar.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import NavbarSkeleton from "../Skeletons/NavbarSkeleton";
-import { Menu, Search } from "lucide-react";
+import { Menu, Search, X } from "lucide-react";
 
 const links = [
   { name: "Home", href: "/" },
@@ -69,7 +69,7 @@ function Navbar() {
                     <li key={href}>
                       <Link
                         href={href}
-                        className={`md:text-lg font-Sofia font-semibold transition-colors duration-300 ${
+                        className={`md:text-lg lg:text-xl font-Sofia font-semibold transition-colors duration-300 ${
                           pathName === href
                             ? "text-red-500 font-bold"
                             : "text-black hover:text-rose-500"
@@ -98,14 +98,23 @@ function Navbar() {
                 </Link>
               </div>
               <div className="md:hidden">
-                <button className="" onClick={() => setOpenModal(!openModal)}>
+                <button
+                  className="py-2 px-3 rounded-2xl border border-rose-500 bg-white text-rose-600 hover:bg-rose-500 backdrop-blur-lg hover:text-white duration-300 hover:scale-105"
+                  onClick={() => setOpenModal(!openModal)}
+                >
                   <Menu />
                 </button>
               </div>
               <div
-                className={`h-screen w-full fixed inset-0 top-0 bg-black/50 border border-black ${openModal ? "left-0" : "-left-500"}`}
+                className={`h-screen w-full fixed inset-0 top-0 bg-black/50 border border-black duration-500 ease-in-out ${openModal ? "left-0" : "-left-200"}`}
               >
-                <div className="flex flex-col justify-between gap-12 p-6 bg-white max-w-xs h-screen">
+                <div className="flex flex-col justify-between gap-12 p-6 bg-rose-50 max-w-xs h-screen relative">
+                  <button
+                    onClick={() => setOpenModal(false)}
+                    className="p-2 rounded-2xl absolute top-2 -right-12 border border-rose-500 bg-rose-500 text-white hover:bg-white/10 backdrop-blur-lg hover:scale-95 hover:text-rose-600 duration-300"
+                  >
+                    <X />
+                  </button>
                   <div className="flex flex-col gap-10">
                     <div className="shrink-0 py-4 flex items-center justify-center">
                       <Link href={"/"}>
@@ -126,7 +135,7 @@ function Navbar() {
                           <li key={href}>
                             <Link
                               href={href}
-                              className={`md:text-lg p-2 w-full inline-block font-Sofia font-semibold transition-colors duration-300 ${
+                              className={`md:text-lg lg:text-xl p-2 w-full inline-block font-Sofia font-semibold transition-colors duration-300 ${
                                 pathName === href
                                   ? "text-red-500 font-bold"
                                   : "text-black hover:text-rose-500"
@@ -143,23 +152,23 @@ function Navbar() {
                         <input
                           type="text"
                           placeholder="Search your menu..."
-                          className="text-sm border border-rose-500 py-2.5 px-4 rounded-2xl focus:outline-rose-500"
+                          className="text-sm border border-rose-500/20 bg-white py-2.5 px-4 rounded-2xl focus:outline-rose-500"
                         />
                         <button className="py-2.5 px-4 border border-rose-500 bg-rose-500 text-white hover:bg-rose-600 rounded-2xl w-full flex items-center  justify-center duration-300">
                           <Search />
                         </button>
                       </div>
                       {/* Action Buttons */}
-                      <div className="flex flex-col gap-4">
+                      <div className="flex flex-row gap-4 w-full">
                         <Link
                           href={"/account/signup"}
-                          className="py-2 px-6 rounded-2xl flex items-center justify-center font-Sofia font-semibold border border-rose-600 bg-rose-500 hover:bg-rose-600 duration-300 hover:shadow text-white whitespace-nowrap"
+                          className="py-2 px-6 rounded-2xl flex items-center justify-center font-Sofia font-semibold outline-2 outline-rose-600 bg-rose-500 hover:bg-rose-600 duration-300 hover:shadow text-white whitespace-nowrap w-full"
                         >
                           Sign Up
                         </Link>
                         <Link
                           href={"/account/signin"}
-                          className="py-2 px-6 rounded-2xl flex items-center justify-center font-Sofia font-semibold border border-rose-600 text-rose-600 hover:bg-rose-200 duration-300 hover:shadow whitespace-nowrap"
+                          className="py-2 px-6 rounded-2xl flex items-center justify-center font-Sofia font-semibold outline-2 outline-rose-600 text-rose-600 hover:bg-rose-500 hover:text-white duration-300 hover:shadow whitespace-nowrap w-full"
                         >
                           Sign In
                         </Link>
@@ -167,7 +176,7 @@ function Navbar() {
                     </div>
                   </div>
                   <div>
-                    <div className="flex items-center gap-4 py-2 px-3.5 rounded-2xl hover:bg-rose-100 hover:-translate-y-0.5 duration-300 shadow-md shadow-rose-500/10 hover:shadow-rose-500/50 bg-rose-500/5 cursor-pointer">
+                    <div className="hidden items-center gap-4 py-2 px-3.5 rounded-2xl hover:bg-rose-100 hover:-translate-y-0.5 duration-300 shadow-md shadow-rose-500/10 hover:shadow-rose-500/50 bg-rose-500/5 cursor-pointer">
                       <div>
                         <Image
                           src={"/logos/user.jpg"}
@@ -183,6 +192,11 @@ function Navbar() {
                           alaminislam4122.bd@gmail.com
                         </p>
                       </div>
+                    </div>
+                    <div>
+                      <button className="py-2 px-6 rounded-2xl font-Sofia font-semibold outline-2 outline-rose-600 bg-rose-50 hover:bg-rose-600/90 duration-300 hover:shadow text-rose-600 hover:text-white whitespace-nowrap w-full hover:-translate-y-0.5 shadow-md shadow-rose-500/10 hover:shadow-rose-500/50">
+                        Log out
+                      </button>
                     </div>
                   </div>
                 </div>
